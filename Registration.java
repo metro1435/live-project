5995a33a3299303cc6e9201b0f2b192913a751f4
co-br <1875461795@qq.com>
2020-03-15 22:17:27 +0800

    Create Registration.java

---
+++ Registration.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class Registration {
+    private String name;
+    private String telNumber;
+    private String id;
+    private int sum;
+    private static int number=0;
+    
+    public Registration(String name_,String telNumber_,String id_,int sum_) {
+        name=name_;
+        telNumber=telNumber_;
+        id=id_;
+        sum=sum_;
+    }
+    public String isLegal()throws Exception{
+        
+      
+         return s;
+        
+    }
+    
+
+}
