775c5fc200b2c2e7fc93d049210540c1f2feca8c
qiuyanHuang <57276289+qiuyanHuang@users.noreply.github.com>
2020-03-15 18:47:22 +0800

    Create User.java

---
+++ User.java
@@ -0,0 +1,26 @@
+class User{
+	private String name;
+	private String idNum;
+	private String phoneNum;
+	private int first;
+	private int second;
+	private int third;
+	private int today;
+
+	public String getName(){return name;}
+	public String getIdNum(){return idNum;}
+	public String getPhoneNum(){return phoneNum;}
+	public int getFirst(){return first;}
+	public int getSecond(){return second;}
+	public int getThird(){return third;}
+	public int getToday(){return today;}
+
+	public void setName(String na){name=na;}
+	public void setIdNum(String id){idNum=id;}
+	public void setPhoneNum(String phone){phoneNum=phone;}
+	public void setFirst(int fi){first=fi;}
+	public void setSecond(int se){second=se;}
+	public void setThird(int th){third=th;}
+	public void setToday(int to){today=to;}
+
+}
