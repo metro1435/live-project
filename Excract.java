6e7998e2ace0f13c872f5f65b64e022bf56105a8
itwillbefine <1227583815@qq.com>
2020-03-15 22:45:04 +0800

    Create Excract.java

---
+++ Excract.java
@@ -0,0 +1,31 @@
+﻿package initial_interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Excract {
+    int maskNum=0;
+     
+    Excract(int num) {
+        maskNum=num;
+    }
+    public String[] getOrderNum() throws Exception {
+    	List<Order> orderList=drawLots();
+    	final int size=orderList.size();
+        List<String> orderId=new ArrayList<String>();
+        for (int i=0;i<size;i++) {
+        	if (orderList.get(i).getBallot()==1) {
+        		orderId.add(orderList.get(i).getOrderID());
+        	}
+        }
+        String[]res = new String[orderList.size()];
+        for(int i=0;i<orderList.size();i++){
+            res[i]=orderList.get(i).getOrderID();
+        }
+        return res;
+    }
+
+}
+
+
+
