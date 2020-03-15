6ddc8047e399d1832d62b72cd04270c16d6b1f3a
sunny <2453709145@qq.com>
2020-03-15 12:54:42 +0800

    增加user表实体类

--- graph.java
+++ graph.java
@@ -5,9 +5,9 @@ public class graph {
 }
 
 class Order{
-	private int orderID;//±àºÅ
-	private String idNum;//Éí·ÝÖ¤ºÅ
-	private String phoneNum;//µç»°ºÅÂë
+	private int orderID;//ï¿½ï¿½ï¿½
+	private String idNum;//ï¿½ï¿½ï¿½Ö¤ï¿½ï¿½
+	private String phoneNum;//ï¿½ç»°ï¿½ï¿½ï¿½ï¿½
 	private int maskNum;
 	private int ballot;
 	
@@ -23,6 +23,31 @@ class Order{
 	public void setBallot(int ba){ballot=ba;}
 }
 
+class User{
+	private String name;
+	private String idNum;
+	private String phoneNum;
+	private int first;
+	private int second;
+	private int third;
+	private int today;
+
+	public String getName(){return name;}
+	public String getIdNum(){return idNum;}
+	public String getPhoneNum(){return phoneNum;}
+	public int getFirst(){return first;}
+	public int getSecond(){return second;}
+	public int getThird(){return third;}
+	public int getToday(){return today;}
+	public void setName(String na){name=na;}
+	public void setIdNum(String id){idNum=id;}
+	public void setPhoneNum(String phone){phoneNum=phone;}
+	public void setFirst(int fi){first=fi;}
+	public void setSecond(int se){second=se;}
+	public void setThird(int th){third=th;}
+	public void setToday(int to){today=to;}
+
+}
 
 
 
